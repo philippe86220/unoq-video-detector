05aceefcee9200c6e7949a5768694937a467419c
philippe86220 <142721115+philippe86220@users.noreply.github.com>
2025-11-17 10:44:26 +0100

    Update sketch.ino

--- code/stm32/sketch.ino
+++ code/stm32/sketch.ino
@@ -1,1 +1,31 @@
+#include <Arduino.h>
+#include <Arduino_RouterBridge.h>
+
+const int ledPin = LED_BUILTIN;
+
+void setLedState(bool state) {
+  // LED active LOW : true = ON -> LOW, false = OFF -> HIGH
+  digitalWrite(ledPin, state ? LOW : HIGH);
+
+  Monitor.print("[C++] setLedState(");
+  Monitor.print(state ? "true" : "false");
+  Monitor.println(")");
+}
+
+void setup() {
+  pinMode(ledPin, OUTPUT);
+
+  // LED éteinte au démarrage
+  digitalWrite(ledPin, HIGH);
+
+  Bridge.begin();
+  Monitor.begin();
+  Bridge.provide("setLedState", setLedState);
+
+  Monitor.println("[C++] setup terminé, LED à OFF");
+}
+
+void loop() {
+  delay(10);
+}
 
